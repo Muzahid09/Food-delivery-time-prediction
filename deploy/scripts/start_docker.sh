cae7934fd21a133900f2652a732f6435ad7efdcd
Muzahid <https://github.com/Muzahid09>
2025-03-24 20:27:01 +0530

    add changes in start_docker.sh file

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-exec > /home/ubuntu/start_docker.log 2>&1
+
 
 # login in ecr
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 730335644260.dkr.ecr.ap-south-1.amazonaws.com
 
 
 #pull the latest image
-docker pull 730335644260.dkr.ecr.ap-south-1.amazonaws.com/food_repo:v1
+docker pull 730335644260.dkr.ecr.ap-south-1.amazonaws.com/delivery_time:v2
 
 
-
-# Check if the container is already running
-if [ "$(docker ps -q -f name=ec2_first_container)" ]; then
-    docker stop ec2_first_container
-fi
-
-if [ "$(docker ps -aq -f name=ec2_first_container)" ]; then
-    docker rm ec2_first_container
-fi
+docker stop model_v2 || true
+docker rm model_v2 || true
 
 
 # Run the Docker container
-sudo docker run -d -p 80:8000 -e DAGSHUB_USER_TOKEN=705171615cb002f197303ee05d8813d5f80e089a --name ec2_first_container 730335644260.dkr.ecr.ap-south-1.amazonaws.com/food_repo:v1
+docker run -d -p 80:8000 -e DAGSHUB_USER_TOKEN=705171615cb002f197303ee05d8813d5f80e089a --name model_v2 730335644260.dkr.ecr.ap-south-1.amazonaws.com/delivery_time:v2
