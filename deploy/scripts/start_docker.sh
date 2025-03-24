1308c4cdc7173ada0d943d3b78b26515d2673f2a
Muzahid <https://github.com/Muzahid09>
2025-03-25 00:23:34 +0530

    added changes in deploy and appspec.yaml

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+# Log everything to start_docker.log
+exec > /home/ubuntu/start_docker.log 2>&1
 
-
-# login in ecr
+echo "Logging in to ECR..."
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 730335644260.dkr.ecr.ap-south-1.amazonaws.com
 
-
-#pull the latest image
+echo "Pulling Docker image..."
 docker pull 730335644260.dkr.ecr.ap-south-1.amazonaws.com/delivery_time:v2
 
+echo "Checking for existing container..."
+if [ "$(docker ps -q -f name=model_v2)" ]; then
+    echo "Stopping existing container..."
+    docker stop model_v2
+fi
 
-docker stop model_v2 || true
-docker rm model_v2 || true
+if [ "$(docker ps -aq -f name=model_v2)" ]; then
+    echo "Removing existing container..."
+    docker rm model_v2
+fi
 
-
-# Run the Docker container
-docker run -d -p 80:8000 -e DAGSHUB_USER_TOKEN=705171615cb002f197303ee05d8813d5f80e089a --name model_v2 730335644260.dkr.ecr.ap-south-1.amazonaws.com/delivery_time:v2
+echo "Starting new container..."
+docker run -d -p 80:8000 --name model_v2 -e DAGSHUB_USER_TOKEN=705171615cb002f197303ee05d8813d5f80e089a 730335644260.dkr.ecr.ap-south-1.amazonaws.com/delivery_time:v2
+echo "Container started successfully."
